691629dcf3a7996c41418f9c7d5448a2a58e5d2b
Aarón <aaronmanceracantador2003@gmail.com>
2024-06-02 21:54:10 +0200

    Haciendo responsivo el menu principañ

--- View/Home.py
+++ View/Home.py
@@ -4,16 +4,44 @@ import flet as ft
 
 def Home(page):
     print("Inicia el home")
-    bienvenida = ft.Column()
-    bienvenida.controls.append(ft.Text("Bienvenido al juego del Ahorcado POO"))
+
+    # bienvenida.controls.append(ft.Text("Bienvenido al juego del Ahorcado POO"))
     def btn_go_to_game(e):
         page.go("/game")
         page.update()
-    bienvenida.controls.append(ft.ElevatedButton(text="Ir para el juego",on_click=btn_go_to_game))
-    
-    content = ft.Column(
+
+    # bienvenida.controls.append(ft.ElevatedButton(text="Ir para el juego",on_click=btn_go_to_game))
+
+    content = ft.ResponsiveRow(
         [
-            bienvenida
+            ft.Container(
+                #https://github.com/flet-dev/flet/discussions/2545
+                content=ft.Column(
+                    controls=[
+                        ft.Image(
+                            src="../assets/Img/test.svg",
+                            fit=ft.ImageFit.CONTAIN,
+                            expand=True,
+                        )
+                    ],
+                    height=200,
+                    expand=True,
+                ),
+                col={"md": 12, "lg": 4, "xl": 3},
+                alignment=ft.alignment.top_center,
+                bgcolor=ft.colors.YELLOW,
+            ),
+            ft.Container(
+                content=ft.Column(
+                    controls=[
+                        ft.ElevatedButton(
+                            text="Ir para el juego", on_click=btn_go_to_game
+                        )
+                    ],
+                ),
+                col={"md": 12, "lg": 8, "xl": 9},
+                bgcolor=ft.colors.GREEN,
+            ),
         ]
     )
     return content
